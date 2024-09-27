7d737f4ff17e00660e446e6b2f26f142f6f81d83
Irgendwer008 <gehamsprak@gmail.com>
2024-09-27 08:29:23 +0200

    Fix and cleanup binary creatiun

--- to_binary.sh
+++
@@ -1,4 +0,0 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
-DIR="${DIR%x}"
-echo $DIR
-pyinstaller --onefile --distpath ./ --clean --exclude-module credentials --add-data "credentials.py:." --name "Oeffentlichkeitsarbeit-Bot-$OSTYPE" "$DIR/main.py"
