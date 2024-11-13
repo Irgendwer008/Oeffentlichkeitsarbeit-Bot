4baf26a527990c1906c3d1db8c96c06f627e32c4
Irgendwer008 <gehamsprak@gmail.com>
2024-11-13 13:52:33 +0100

    update linux binaries

--- to_binary_scripts/to_binary.sh
+++ to_binary_scripts/to_binary.sh
@@ -1,3 +1,2 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
-pyinstaller --onefile --distpath ../ --clean --exclude-module credentials --name Oeffentlichkeitsarbeit-Bot-$OSTYPE ${DIR%x}/../main.py
-rm Oeffentlichkeitsarbeit-Bot-$OSTYPE.spec
+pyinstaller --onefile --distpath ../ --clean --exclude-module credentials --name Oeffentlichkeitsarbeit-Bot-$OSTYPE ${DIR%x}/../main.py && rm Oeffentlichkeitsarbeit-Bot-$OSTYPE.spec
